519d31e999aa81efcdcf58fbdc5f0b70bdd18187
Dmitry <zeonlb426@gmail.com>
2023-05-02 21:00:44 +0300

    Add video component

--- src/Components/VideoView.js
+++ src/Components/VideoView.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import { PlayIcon, VideoCameraSlashIcon } from '@heroicons/react/24/outline';
-import { YouTubePlayer } from "react-youtube";
+import { PlayIcon} from '@heroicons/react/24/outline';
+import YouTube from 'react-youtube'
 
 
 
@@ -8,10 +8,20 @@ export default function VideoView() {
 
     const [isOpen, setOpen] = useState(false)
     const [listTv, setListTv] = useState([])
+    const [index, setIndex] = useState(0)
+    const [videoList, setVideoList] = useState([])
+    const [isLoadingVideo, setLoadingVideo] = useState(true)
+    const [opts, setOpts] = useState({
+        height: '600',
+        width: '100%',
+        playerVars: {
+          // https://developers.google.com/youtube/player_parameters
+          autoplay: 1,
+        }})
     const [isLoadinglistTv, setIsLoadinglistTv] = useState(true)
 
     const listTvUrl = 'https://api.themoviedb.org/3/tv/airing_today?api_key=46b3d80e68c3305b185dc8a255c58fac&language=en-US&page=2'
-    
+
     useEffect(
         () => {
             fetch(listTvUrl)
@@ -19,52 +29,71 @@ export default function VideoView() {
                 .then(answer => {
                     setListTv(answer.results)
                     setIsLoadinglistTv(false)
+                    setIndex( Math.round(Math.random()*19) )
                 })
                
         }, [])
 
-        // useEffect(
-        //     () => {
-        //         if(!isLoadinglistTv){
-        //         const videoId = `https://api.themoviedb.org/3/tv/${listTv[randomlistTv].id}/videos?api_key=46b3d80e68c3305b185dc8a255c58fac&language=en-US`
-        //          fetch(videoId)
-        //             .then(response => response.json())
-        //             .then(answer => {
-        //                 setListTv(answer.results)
-        //                 setIsLoadinglistTv(false)
-        //             })
-        //         }   
-        //     }, [listTv])
+        useEffect(
+            () => {
+                if(!isLoadinglistTv){
+                const videoId = `https://api.themoviedb.org/3/tv/${listTv[index].id}/videos?api_key=46b3d80e68c3305b185dc8a255c58fac&language=en-US`
+                 fetch(videoId)
+                    .then(response => response.json())
+                    .then(answer => {
+                        if(answer.results.length  === 0){
+                            setIndex( Math.round(Math.random()*19) )
+                        }else{
+                            setVideoList(answer.results)
+                            // console.log(answer.results);
+                            setLoadingVideo(false)
+                        }
+                        
+                    })
+                }   
+            }, [listTv, index])
+
+            
 
 
     if (isLoadinglistTv) {
         return (<div>Загружается...</div>)
     }
-    const videoId = `https://api.themoviedb.org/3/tv/${listTv[randomlistTv].id}/videos?api_key=46b3d80e68c3305b185dc8a255c58fac&language=en-US`
+    // const randomlistTv = Math.round(Math.random()*10)
+    // const videoId = `https://api.themoviedb.org/3/tv/${listTv[index].id}/videos?api_key=46b3d80e68c3305b185dc8a255c58fac&language=en-US`
 
+   
+    let url = 'https://user-images.githubusercontent.com/19985451/40033362-7273cdce-583b-11e8-8534-583a01fa6cbc.png'
 
-    const randomlistTv = Math.round(Math.random()*10)
-
-
-   const url = listTv[randomlistTv].backdrop_path ? 'https://image.tmdb.org/t/p/original' + listTv[randomlistTv].backdrop_path : 'https://cdn.dnaindia.com/sites/default/files/styles/full/public/1545128.jpg'
-
+    if (typeof listTv[index] === 'object' &&
+        listTv[index].hasOwnProperty('backdrop_path')) {
+        
+         url = 'https://image.tmdb.org/t/p/original' + listTv[index].backdrop_path 
+      }
+    
     return (
-        <div className="max-w-screen-2xl mx-auto mt-40 border-2 border-white">
-            <div className="border-4 border-red-500 h-[600px] p-10 flex items-end relative bg-center bg-cover" style={{backgroundImage: `url(${url})`}}>
+        <div className="max-w-screen-2xl mx-auto mt-40  ">
+            
+            {isOpen && !isLoadingVideo ? 
+            <YouTube className="h-[600px] p-10 flex items-end relative" videoId={videoList[0].key} opts={opts} 
+            onPause={() => setOpen(false)} 
+            onEnd={() => setOpen(false)}/>
+            :
+            <div className="h-[600px] p-10 flex items-end relative bg-center bg-cover" style={{backgroundImage: `url(${url})`}}>
                 <div onClick={() => setOpen(true)} className='flex items-center justify-center border-2 border-yellow-300 bg-transparent w-20 h-20 rounded-full cursor-pointer absolute inset-y-1/2 inset-x-1/2 -translate-y-1/2 -translate-x-1/2'>
-                    <YouTube videoId="listTv[randomlistTv].key" opts={opts} onReady={this._onReady} />
+                    
                     <div className="flex items-center justify-center bg-yellow-300 w-14 h-14 rounded-full">
                         <PlayIcon className="h-6 w-6 text-black" />
                     </div>
                 </div>
 
-                <div className="grid gap-4">
+                <div className=" gap-4">
 
-                    <h2 className=" text-6xl text-black font-bold">{listTv[randomlistTv].name}</h2>
-                    <p>{listTv[randomlistTv].overview}</p>
+                    <h2 className="inline-flex text-6xl text-black bg-yellow-300 font-bold">{listTv[index].name}</h2>
+                    <p className="block text-black bg-yellow-300 mt-3">{listTv[index].overview}</p>
                 </div>
 
-            </div>
+            </div>}
 
             {/* <div className="border-4 border-orange-500 h-[400px] p-10 flex">
                 <p>video</p>
